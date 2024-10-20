91d2bb6670dcc959ffb678db2c5be5fbbf8a9c50
ssummerhays <spencersummerhays3@gmail.com>
2024-10-19 23:26:18 -0600

    Implemented initial version of listGames, createGame, and JoinGame

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,14 +1,49 @@
 package service;
 
+import chess.ChessGame;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDataAccess;
+import dataaccess.MemoryGameDataAccess;
 import model.AuthData;
 import model.GameData;
+import service.requests.CreateGameRequest;
+import service.requests.JoinGameRequest;
+import service.requests.ListGamesRequest;
 
 import java.util.Collection;
 
 public class GameService {
-  public Collection<GameData> listGames(AuthData authData) {}
+  public Collection<GameData> listGames(ListGamesRequest listGamesRequest) throws DataAccessException {
+    String authToken = listGamesRequest.authToken();
+    MemoryAuthDataAccess authDAO = new MemoryAuthDataAccess();
+    AuthData authData = authDAO.getAuth(authToken);
 
-  public GameData createGame(AuthData authData, GameData gameData) {}
+    MemoryGameDataAccess gameDAO = new MemoryGameDataAccess();
+    return gameDAO.getGames();
+  }
 
-  public void joinGame(AuthData authData, GameData gameData) {}
+  public int createGame(CreateGameRequest createGameRequest) throws DataAccessException {
+    String authToken = createGameRequest.authToken();
+    String gameName = createGameRequest.gameName();
+
+    MemoryAuthDataAccess authDAO = new MemoryAuthDataAccess();
+    AuthData authData = authDAO.getAuth(authToken);
+
+    MemoryGameDataAccess gameDAO = new MemoryGameDataAccess();
+    return gameDAO.createGame(gameName);
+  }
+
+  public void joinGame(JoinGameRequest joinGameRequest) throws DataAccessException{
+    String authToken = joinGameRequest.authToken();
+    ChessGame.TeamColor playerColor = joinGameRequest.playerColor();
+    int gameID = joinGameRequest.gameID();
+
+    MemoryAuthDataAccess authDAO = new MemoryAuthDataAccess();
+    AuthData authData = authDAO.getAuth(authToken);
+
+    MemoryGameDataAccess gameDAO = new MemoryGameDataAccess();
+    GameData gameData = gameDAO.getGame(gameID);
+
+    gameDAO.joinGame(gameData, authData.username(), playerColor);
+  }
 }
