03a739f0a879a0c98067f93360148e1a98f41e34
ssummerhays <spencersummerhays3@gmail.com>
2024-10-21 22:01:12 -0600

    Create skeleton for service tests

---
+++ server/src/test/java/service/GameServiceTest.java
@@ -0,0 +1,44 @@
+package service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameServiceTest {
+  @Test
+  @DisplayName("Successfully list games")
+  public void listGamesSuccess() {
+
+  }
+
+  @Test
+  @DisplayName("Unauthorized list games")
+  public void listGamesFail() {
+
+  }
+
+  @Test
+  @DisplayName("Successfully create new game")
+  public void createGameSuccess() {
+
+  }
+
+  @Test
+  @DisplayName("Unsuccessfully create game with no gameName")
+  public void createGameFail() {
+
+  }
+
+  @Test
+  @DisplayName("Successfully join game")
+  public void joinGameSuccess() {
+
+  }
+
+  @Test
+  @DisplayName("Unsuccessfully join game with no playerColor")
+  public void joinGameFail() {
+
+  }
+}
---
+++ server/src/test/java/service/UserServiceTest.java
@@ -0,0 +1,50 @@
+package service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+  @Test
+  @DisplayName("Successfully register new user")
+  public void registerSuccess() {
+
+  }
+
+  @Test
+  @DisplayName("Unsuccessfully register user with no password")
+  public void registerFail() {
+
+  }
+
+  @Test
+  @DisplayName("Successfully login existing user")
+  public void loginSuccess() {
+
+  }
+
+  @Test
+  @DisplayName("Unsuccessfully login user that does not exist")
+  public void loginFail() {
+
+  }
+
+  @Test
+  @DisplayName("Successfully logout user")
+  public void logoutSuccess() {
+
+  }
+
+  @Test
+  @DisplayName("Unsuccessfully log out a logged out user")
+  public void logoutFail() {
+
+  }
+
+  @Test
+  @DisplayName("Test Clear")
+  public void clearTest() {
+
+  }
+}
