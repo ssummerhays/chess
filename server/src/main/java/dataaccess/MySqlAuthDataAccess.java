4bad5631da38f1bf77dfb0085a94d54e733ac2cf
ssummerhays <spencersummerhays3@gmail.com>
2024-10-31 12:08:15 -0600

    Implemented constructor for MySqlAuthDataAccess and implemented getAuth

--- server/src/main/java/dataaccess/MySqlAuthDataAccess.java
+++ server/src/main/java/dataaccess/MySqlAuthDataAccess.java
@@ -1,10 +1,46 @@
 package dataaccess;
 
 import model.AuthData;
+import model.UserData;
+
+import java.sql.SQLException;
 
 public class MySqlAuthDataAccess implements AuthDataAccess {
+
+  public MySqlAuthDataAccess() throws DataAccessException {
+    DatabaseManager.createDatabase();
+    try (var conn = DatabaseManager.getConnection()) {
+      String[] createStatements={
+              """
+            CREATE TABLE IF NOT EXISTS  authData (
+              `authToken` varchar(256) NOT NULL UNIQUE,
+              `username` varchar(256) NOT NULL UNIQUE,
+              PRIMARY KEY (`authToken`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+      };
+      for (var statement : createStatements) {
+        try (var preparedStatement = conn.prepareStatement(statement)) {
+          preparedStatement.executeUpdate();
+        }
+      }
+    } catch (SQLException e) {
+      throw new DataAccessException("Unable to configure database");
+    }
+  }
   public AuthData getAuth(String authToken) throws DataAccessException {
-    return null;
+    try (var conn = DatabaseManager.getConnection()) {
+      String statement = "SELECT authToken, username FROM authData WHERE authToken=?";
+      try (var preparedStatement = conn.prepareStatement(statement)) {
+        preparedStatement.setString(1, authToken);
+        var rs = preparedStatement.executeQuery();
+        rs.next();
+        String username = rs.getString(2);
+        return new AuthData(authToken, username);
+      }
+    } catch (SQLException e) {
+      throw new DataAccessException("Error: unauthorized");
+    }
   }
 
   public void createAuth(AuthData authData) throws DataAccessException {
