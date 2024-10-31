1fe5efca66d1977bc4d26a76559e1724753347bb
ssummerhays <spencersummerhays3@gmail.com>
2024-10-30 22:02:44 -0600

    Implemented first version of getUser for MySqlUserDataAccess

--- server/src/main/java/dataaccess/MySqlUserDataAccess.java
+++ server/src/main/java/dataaccess/MySqlUserDataAccess.java
@@ -2,9 +2,23 @@ package dataaccess;
 
 import model.UserData;
 
+import java.sql.SQLException;
+
 public class MySqlUserDataAccess implements UserDataAccess {
   public UserData getUser(String username) throws DataAccessException {
-    return null;
+    try (var conn = DatabaseManager.getConnection();) {
+      String statement = "SELECT username, password, email FROM chess WHERE username=?";
+      try (var preparedStatement = conn.prepareStatement(statement)) {
+        preparedStatement.setString(1, username);
+        var rs = preparedStatement.executeQuery();
+        rs.next();
+        String hashed = rs.getString(2);
+        String email = rs.getString(3);
+        return new UserData(username, hashed, email);
+      }
+    } catch (SQLException e) {
+      throw new DataAccessException("Database Error");
+    }
   }
 
   public void createUser(UserData userData) throws DataAccessException {
