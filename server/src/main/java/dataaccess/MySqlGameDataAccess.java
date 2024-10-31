5498103bb969015d2f0bb81e1eccb03a18e4c4c8
ssummerhays <spencersummerhays3@gmail.com>
2024-10-31 15:06:19 -0600

    Implemented a constructor for MySqlGameDataAccess and implemented createGame in MySqlGameDataAccess

--- server/src/main/java/dataaccess/MySqlGameDataAccess.java
+++ server/src/main/java/dataaccess/MySqlGameDataAccess.java
@@ -1,12 +1,41 @@
 package dataaccess;
 
 import chess.ChessGame;
+import com.google.gson.Gson;
 import model.GameData;
 import model.PrintedGameData;
+import org.mindrot.jbcrypt.BCrypt;
 
+import java.sql.SQLException;
 import java.util.Collection;
 
 public class MySqlGameDataAccess implements GameDataAccess {
+  int gameID = 1;
+
+  public MySqlGameDataAccess() throws DataAccessException{
+    DatabaseManager.createDatabase();
+    try (var conn = DatabaseManager.getConnection()) {
+      String[] createStatements={
+              """
+            CREATE TABLE IF NOT EXISTS  gameData (
+              `gameID` int NOT NULL,
+              `whiteUsername` varchar(256),
+              `blackUsername` varchar(256),
+              `gameName` varchar(256) NOT NULL UNIQUE,
+              `gameJSON` varchar(256) NOT NULL,
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+      };
+      for (var statement : createStatements) {
+        try (var preparedStatement = conn.prepareStatement(statement)) {
+          preparedStatement.executeUpdate();
+        }
+      }
+    } catch (SQLException e) {
+      throw new DataAccessException("Unable to configure database");
+    }
+  }
   public Collection<PrintedGameData> getGames() throws DataAccessException {
     return null;
   }
@@ -16,7 +45,26 @@ public class MySqlGameDataAccess implements GameDataAccess {
   }
 
   public int createGame(String gameName) throws DataAccessException {
-    return 0;
+    try (var conn = DatabaseManager.getConnection()) {
+      String statement = "INSERT INTO gameData (gameID, whiteUsername, blackUsername, gameName, gameJSON) VALUES (?, NULL, NULL, ?, ?)";
+      try (var preparedStatement = conn.prepareStatement(statement)) {
+        preparedStatement.setInt(1, gameID);
+        int prevGameID = gameID;
+        gameID++;
+        preparedStatement.setString(2, gameName);
+        ChessGame newGame = new ChessGame();
+        String jsonGame = new Gson().toJson(newGame);
+        preparedStatement.setString(1, jsonGame);
+        preparedStatement.executeUpdate();
+        return prevGameID;
+      }
+    } catch (SQLException e) {
+      if (e.getMessage().contains("Duplicate")) {
+        throw new DataAccessException("Error: already taken");
+      } else {
+        throw new DataAccessException("Error: bad request");
+      }
+    }
   }
   
   public void joinGame(GameData gameData, String username, ChessGame.TeamColor teamColor) throws DataAccessException {
