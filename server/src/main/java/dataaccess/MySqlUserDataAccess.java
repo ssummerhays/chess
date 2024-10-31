4f3102598db15fd7d5b0c5090e73d7b61da48520
ssummerhays <spencersummerhays3@gmail.com>
2024-10-30 22:11:53 -0600

    Implemented first version of createUser for MySqlUserDataAccess

--- server/src/main/java/dataaccess/MySqlUserDataAccess.java
+++ server/src/main/java/dataaccess/MySqlUserDataAccess.java
@@ -1,13 +1,14 @@
 package dataaccess;
 
 import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.sql.SQLException;
 
 public class MySqlUserDataAccess implements UserDataAccess {
   public UserData getUser(String username) throws DataAccessException {
     try (var conn = DatabaseManager.getConnection();) {
-      String statement = "SELECT username, password, email FROM chess WHERE username=?";
+      String statement = "SELECT username, password, email FROM userData WHERE username=?";
       try (var preparedStatement = conn.prepareStatement(statement)) {
         preparedStatement.setString(1, username);
         var rs = preparedStatement.executeQuery();
@@ -22,6 +23,18 @@ public class MySqlUserDataAccess implements UserDataAccess {
   }
 
   public void createUser(UserData userData) throws DataAccessException {
+    try (var conn = DatabaseManager.getConnection();) {
+      String statement = "INSERT INTO userData (username, password, email) VALUES (?, ?, ?)";
+      try (var preparedStatement = conn.prepareStatement(statement)) {
+        preparedStatement.setString(1, userData.username());
+        String hashed = BCrypt.hashpw(userData.password(), BCrypt.gensalt());
+        preparedStatement.setString(2, hashed);
+        preparedStatement.setString(3, userData.email());
+        preparedStatement.executeQuery();
+      }
+    } catch (SQLException e) {
+      throw new DataAccessException("Database Error");
+    }
 
   }
 
