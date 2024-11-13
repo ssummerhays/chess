f7f25cab4ce663c5c1bff4a18117ffd33bc40bbe
ssummerhays <spencersummerhays3@gmail.com>
2024-11-13 11:17:04 -0700

    Created a PreLoginRepl class and implemented the run method.

---
+++ client/src/main/java/ui/PreLoginRepl.java
@@ -0,0 +1,32 @@
+package ui;
+
+import java.util.Scanner;
+
+public class PreLoginRepl {
+  private final ChessClient client;
+
+  public PreLoginRepl(String serverURL) { client = new ChessClient(serverURL);}
+
+  public void run() {
+    System.out.println("♕ Welcome to Chess. Log In to start");
+    System.out.print(client.help());
+
+    Scanner scanner = new Scanner(System.in);
+    var result = "";
+    while (!result.equals("quit")) {
+      printPrompt();
+      String line = scanner.nextLine();
+
+      try {
+        result = client.eval(line);
+        System.out.print(EscapeSequences.SET_TEXT_COLOR_BLUE + result);
+      } catch (Throwable e) {
+        var msg = e.toString();
+        System.out.print(msg);
+      }
+      System.out.println();
+    }
+  }
+
+  private void printPrompt() { System.out.print("\n" + EscapeSequences.RESET_TEXT_COLOR + ">>> " + EscapeSequences.SET_TEXT_COLOR_GREEN); }
+}
