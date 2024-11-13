98b5bb985e0a54cd93bad222abc6b5757165a031
ssummerhays <spencersummerhays3@gmail.com>
2024-11-13 12:14:51 -0700

    Minor updates to the PreLoginRepl

--- client/src/main/java/ui/PreLoginRepl.java
+++ client/src/main/java/ui/PreLoginRepl.java
@@ -8,8 +8,7 @@ public class PreLoginRepl {
   public PreLoginRepl(String serverURL) { client = new ChessClient(serverURL);}
 
   public void run() {
-    System.out.println("♕ Welcome to Chess. Log In to start");
-    System.out.print(client.help());
+    System.out.println("♕ Welcome to 240 Chess. Type help to begin");
 
     Scanner scanner = new Scanner(System.in);
     var result = "";
@@ -28,5 +27,5 @@ public class PreLoginRepl {
     }
   }
 
-  private void printPrompt() { System.out.print("\n" + EscapeSequences.RESET_TEXT_COLOR + ">>> " + EscapeSequences.SET_TEXT_COLOR_GREEN); }
+  private void printPrompt() { System.out.print("\n" + EscapeSequences.RESET_TEXT_COLOR + "[LOGGED_OUT] >>> "); }
 }
