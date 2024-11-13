f8e4f62a3dee7ec54b912155a169bfaa42e97aa0
ssummerhays <spencersummerhays3@gmail.com>
2024-11-13 13:11:18 -0700

    Updated Client Main to start a server using the provided port or 8080 by default and stop the server after main terminates

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,12 +1,21 @@
-import ui.PreLoginRepl;
+import server.Server;
+import ui.Repl;
 
 public class Main {
     public static void main(String[] args) {
-        var serverURL = "http://localhost:8080";
+        int port = 8080;
+        if (args.length >= 1) {
+            port = Integer.parseInt(args[0]);
+        }
+
+        Server server = new Server();
+        server.run(port);
+        var serverURL = "http://localhost:" + port;
         if (args.length == 1) {
             serverURL = args[0];
         }
 
-        new PreLoginRepl(serverURL).run();
+        new Repl(serverURL).run();
+        server.stop();
     }
 }
