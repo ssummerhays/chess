f51230103e4a2d87d652e9b616914a873de2c71f
ssummerhays <spencersummerhays3@gmail.com>
2024-11-13 12:14:24 -0700

    Implemented eval and help methods for ChessClient

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -2,6 +2,8 @@ package ui;
 
 import server.ServerFacade;
 
+import java.util.Arrays;
+
 public class ChessClient {
   private final ServerFacade serverFacade;
   private final String serverURL;
@@ -13,15 +15,41 @@ public class ChessClient {
   }
 
   public String eval(String input) {
-    return null;
+    try {
+      var tokens = input.toLowerCase().split(" ");
+      var cmd = (tokens.length > 0) ? tokens[0] : "help";
+      var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+      return switch (cmd) {
+        case "help" -> help();
+        case "quit" -> "quit";
+        case "login" -> logIn(params);
+        case "register" -> register(params);
+        case "logout" -> logOut();
+        case "create" -> createGame(params);
+        case "list" -> listGames();
+        case "join" -> joinGamePlayer(params);
+        case "observe" -> observeGame(params);
+        default -> help();
+      };
+    } catch (Exception e) {
+      return e.getMessage();
+    }
   }
 
   public String help() {
-    return null;
-  }
-
-  public String quit() {
-    return null;
+    if (state == State.SIGNED_OUT) {
+      return "register <USERNAME> <PASSWORD> <EMAIL> " + EscapeSequences.SET_TEXT_COLOR_MAGENTA + "- to create an account\n" +
+              EscapeSequences.SET_TEXT_COLOR_BLUE + "login <USERNAME> <PASSWORD> " + EscapeSequences.SET_TEXT_COLOR_MAGENTA + "- to play chess\n" +
+              EscapeSequences.SET_TEXT_COLOR_BLUE + "quit " + EscapeSequences.SET_TEXT_COLOR_MAGENTA + "- playing chess\n" +
+              EscapeSequences.SET_TEXT_COLOR_BLUE + "help " + EscapeSequences.SET_TEXT_COLOR_MAGENTA + "- with possible commands\n";
+    }
+    return "create <NAME> " + EscapeSequences.RESET_TEXT_COLOR + "- a game\n" +
+            EscapeSequences.SET_TEXT_COLOR_BLUE + "list " + EscapeSequences.SET_TEXT_COLOR_MAGENTA + " games\n" +
+            EscapeSequences.SET_TEXT_COLOR_BLUE + "join <ID> [WHITE|BLACK] " + EscapeSequences.SET_TEXT_COLOR_MAGENTA + "- a game\n" +
+            EscapeSequences.SET_TEXT_COLOR_BLUE + "observe <ID> " + EscapeSequences.SET_TEXT_COLOR_MAGENTA + "- a game\n" +
+            EscapeSequences.SET_TEXT_COLOR_BLUE + "logout " + EscapeSequences.SET_TEXT_COLOR_MAGENTA + "- when you are done\n" +
+            EscapeSequences.SET_TEXT_COLOR_BLUE + "quit " + EscapeSequences.SET_TEXT_COLOR_MAGENTA + "- playing chess\n" +
+            EscapeSequences.SET_TEXT_COLOR_BLUE + "help " + EscapeSequences.SET_TEXT_COLOR_MAGENTA + "- with possible commands\n";
   }
 
   public String logIn(String... params) {
