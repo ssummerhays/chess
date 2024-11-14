455715aeee4780b5d3da3a41087954ea3eddb614
ssummerhays <spencersummerhays3@gmail.com>
2024-11-14 14:19:29 -0700

    updated client Main.java to accept param arguments for a port

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         int port = 0;
 
+        if (args.length >= 1) {
+            port = Integer.parseInt(args[0]);
+        }
+
         String serverURL = "http://localhost:" + port;
 
 
