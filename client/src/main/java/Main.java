65cf3b384f5ebaeee7904d6296b471519c61cd88
ssummerhays <spencersummerhays3@gmail.com>
2024-11-14 00:41:54 -0700

    update

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,17 +1,12 @@
-import server.Server;
 import ui.Repl;
 
 public class Main {
     public static void main(String[] args) {
-
-        Server server = new Server();
-        int port = server.run(0);
+        int port = 0;
 
         String serverURL = "http://localhost:" + port;
 
 
         new Repl(serverURL).run();
-
-        server.stop();
     }
 }
