77a3b995b7f28af6f11d084be7827362cc4181f2
ssummerhays <spencersummerhays3@gmail.com>
2024-11-14 00:26:44 -0700

    update spacing

--- shared/src/main/java/serverHelper/ServerHelper.java
+++ shared/src/main/java/serverHelper/ServerHelper.java
@@ -8,10 +8,12 @@ public class ServerHelper {
   public ServerHelper() {
     server = new Server();
   }
+
   public String runServer(int port) {
     port = server.run(port);
     return "http://localhost:" + port;
   }
+
   public void stopServer() {
     server.stop();
   }
