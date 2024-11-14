17a54c3b3037cded05328ff362f74ec8b6b4669f
ssummerhays <spencersummerhays3@gmail.com>
2024-11-13 21:18:17 -0700

    Updated Main to not have a hardcoded port

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -4,13 +4,13 @@ import ui.Repl;
 
 public class Main {
     public static void main(String[] args) {
-        int port = 8080;
+        int port = 0;
         if (args.length >= 1) {
             port = Integer.parseInt(args[0]);
         }
 
         Server server = new Server();
-        server.run(port);
+        port = server.run(port);
         var serverURL = "http://localhost:" + port;
         if (args.length == 1) {
             serverURL = args[0];
