7ec6d503cad62139acbfc7fad0c1a869bf42b641
ssummerhays <spencersummerhays3@gmail.com>
2024-11-30 18:20:39 -0700

    Implemented highlight moves command

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,26 +1,28 @@
 package ui;
 
 import chess.ChessGame;
+import chess.ChessMove;
 import chess.ChessPiece;
+import chess.ChessPosition;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 
+import model.GameData;
 import model.PrintedGameData;
 import model.UserData;
 import serverfacade.ServerFacade;
 
 import java.lang.reflect.Type;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.Objects;
+import java.util.*;
 
 import static ui.EscapeSequences.*;
 
 public class ChessClient {
   private final ServerFacade serverFacade;
   private final String serverURL;
+  public int currentGameID;
+  public ChessGame.TeamColor currentColor;
   public State state = State.LOGGED_OUT;
   private String authToken = null;
   private PrintedGameData[] gameListArray;
@@ -46,6 +48,7 @@ public class ChessClient {
         case "join" -> joinGamePlayer(params);
         case "observe" -> observeGame(params);
         case "leave" -> leaveGame();
+        case "highlight" -> highlightMoves(params);
         default -> help();
       };
     } catch (Exception e) {
@@ -59,7 +62,7 @@ public class ChessClient {
               EscapeSequences.SET_TEXT_COLOR_BLUE + "login <USERNAME> <PASSWORD> " + EscapeSequences.SET_TEXT_COLOR_MAGENTA + "- to play chess\n" +
               EscapeSequences.SET_TEXT_COLOR_BLUE + "quit " + EscapeSequences.SET_TEXT_COLOR_MAGENTA + "- playing chess\n" +
               EscapeSequences.SET_TEXT_COLOR_BLUE + "help " + EscapeSequences.SET_TEXT_COLOR_MAGENTA + "- with possible commands\n";
-    } else if (state == State.IN_GAME) {
+    } else if (state == State.IN_GAME_PlAYER || state == State.IN_GAME_OBSERVER) {
       return "redraw " + SET_TEXT_COLOR_MAGENTA + "- the chessboard\n" +
               EscapeSequences.SET_TEXT_COLOR_BLUE + "leave " + EscapeSequences.SET_TEXT_COLOR_MAGENTA + "- the game\n" +
               EscapeSequences.SET_TEXT_COLOR_BLUE + "highlight " + EscapeSequences.SET_TEXT_COLOR_MAGENTA + "- legal moves\n" +
@@ -182,8 +185,10 @@ public class ChessClient {
 
       serverFacade.joinGame(authToken, teamColorStr, gameID);
 
-      state = State.IN_GAME;
-      return printGame(gameID, teamColor) + printGame(gameID, oppositeColor);
+      state = State.IN_GAME_PlAYER;
+      currentGameID = gameID;
+      currentColor = teamColor;
+      return printGame(gameID, teamColor);
     }
     throw new ResponseException(400, "Expected: <gameID> [WHITE|BLACK]");
   }
@@ -200,24 +205,54 @@ public class ChessClient {
       } else if (gameID > gameListArray.length) {
         throw new ResponseException(400, "No game with this id exists");
       }
-      state = State.IN_GAME;
-      return printGame(gameID, ChessGame.TeamColor.WHITE) + printGame(gameID, ChessGame.TeamColor.BLACK);
+      state = State.IN_GAME_OBSERVER;
+      return printGame(gameID, ChessGame.TeamColor.WHITE);
     }
     throw new ResponseException(400, "Expected: <gameID>");
   }
 
   public String leaveGame() throws ResponseException {
     assertLoggedIn();
-    if (state != State.IN_GAME) {
-      state = State.LOGGED_IN;
-      return "Successfully left game. Type help for more commands";
+    assertInGame();
+    state = State.LOGGED_IN;
+    return "Successfully left game. Type help for more commands";
+  }
+
+  public String highlightMoves(String... params) throws ResponseException {
+    assertLoggedIn();
+    assertPlayer();
+    if (params.length == 1) {
+      String positionStr = params[0];
+      if (positionStr.length() == 2) {
+        char letter = positionStr.charAt(0);
+        char number = positionStr.charAt(1);
+        if (!Character.isLetter(letter) || !Character.isDigit(number)) {
+          throw new ResponseException(400, "Error: position should be one letter followed by one number");
+        }
+        return printHighlighted(currentGameID, currentColor, letter, number);
+      } else {
+        throw new ResponseException(400, "Error: position should be one letter followed by one number");
+      }
+    } else {
+      throw new ResponseException(400, "Expected: <Position>");
     }
-    throw new ResponseException(400, "Error: not in game");
   }
 
   private void assertLoggedIn() throws ResponseException {
     if (state == State.LOGGED_OUT) {
-      throw new ResponseException(400, "you must sign in");
+      throw new ResponseException(400, "Error: you must sign in");
+    }
+  }
+
+  private void assertPlayer() throws ResponseException {
+    if (state != State.IN_GAME_PlAYER) {
+      throw new ResponseException(400, "Error: you are not currently in game as a player");
+    }
+  }
+
+  private void assertInGame() throws ResponseException {
+    if (state != State.IN_GAME_PlAYER && state != State.IN_GAME_OBSERVER) {
+      throw new ResponseException(400, "Error: you are not currently in a game");
     }
   }
 
@@ -250,6 +285,87 @@ public class ChessClient {
         }
         String squareResult;
         String setBgColor= ((rowNum + c) % 2 == 0)? SET_BG_COLOR_WHITE : SET_BG_COLOR_MAGENTA;
+
+        squareResult = setBgColor + " " + pieceStr + " ";
+        rowStr += squareResult;
+      }
+      rowStr += SET_BG_COLOR_LIGHT_GREY + " " + rowNum + " " + RESET_BG_COLOR + "\n";
+      printedBoard += rowStr;
+    }
+    printedBoard += firstLastRow;
+
+    return printedBoard;
+  }
+
+  private String printHighlighted(int gameID, ChessGame.TeamColor color, char letter, char number) throws ResponseException {
+    ChessGame game = new ChessGame();
+    String firstLastRow = (color == ChessGame.TeamColor.WHITE)?
+            SET_BG_COLOR_LIGHT_GREY + EMPTY + SET_TEXT_COLOR_BLACK + "  a" + EMPTY + " b" + EMPTY + " c" + EMPTY + " d" + EMPTY + " e" +
+                    EMPTY + " f" + EMPTY + " g" + EMPTY + " h  " + EMPTY + RESET_BG_COLOR + "\n"
+            : SET_BG_COLOR_LIGHT_GREY + EMPTY + SET_TEXT_COLOR_BLACK + "  h" + EMPTY + " g" + EMPTY + " f" + EMPTY + " e" + EMPTY + " d" +
+            EMPTY + " c" + EMPTY + " b" + EMPTY + " a  " + EMPTY + RESET_BG_COLOR +"\n";
+    String printedBoard = "\n" + firstLastRow;
+    int pieceRow = Character.getNumericValue(number);
+    int pieceColumn;
+    switch (letter) {
+      case 'h' -> pieceColumn = 8;
+      case 'g' -> pieceColumn = 7;
+      case 'f' -> pieceColumn = 6;
+      case 'e' -> pieceColumn = 5;
+      case 'd' -> pieceColumn = 4;
+      case 'c' -> pieceColumn = 3;
+      case 'b' -> pieceColumn = 2;
+      case 'a' -> pieceColumn = 1;
+      default -> pieceColumn = -1;
+    }
+    if (pieceRow < 1 || pieceRow > 8 || pieceColumn == -1) {
+      throw new ResponseException(400, "Error: invalid position");
+    }
+
+    Collection<ChessMove> validMoves = new HashSet<>();
+    ChessPosition highlightPosition = new ChessPosition(pieceRow, pieceColumn);
+    if (game.chessBoard.squares[pieceRow - 1][pieceColumn - 1] != null) {
+      validMoves = game.validMoves(highlightPosition);
+    }
+
+    for (int r = (color == ChessGame.TeamColor.WHITE)? 7 : 0; (color == ChessGame.TeamColor.WHITE)? r >= 0 : r < 8;
+         r = (color == ChessGame.TeamColor.WHITE)? r - 1 : r + 1) {
+      int rowNum = r + 1;
+      String rowStr = SET_BG_COLOR_LIGHT_GREY + " " + rowNum + " ";
+      var row = game.chessBoard.squares[r];
+      for (int c = (color == ChessGame.TeamColor.BLACK)? 7 : 0; (color == ChessGame.TeamColor.BLACK)? c >= 0 : c < 8;
+           c = (color == ChessGame.TeamColor.BLACK)? c - 1 : c + 1) {
+        ChessPiece piece = row[c];
+        ChessPiece.PieceType type = (piece != null)? piece.getPieceType() : null;
+        String pieceStr;
+        switch (type) {
+          case KING -> pieceStr = (piece.getTeamColor() == ChessGame.TeamColor.WHITE)? WHITE_KING : BLACK_KING;
+          case QUEEN -> pieceStr = (piece.getTeamColor() == ChessGame.TeamColor.WHITE)? WHITE_QUEEN : BLACK_QUEEN;
+          case BISHOP -> pieceStr = (piece.getTeamColor() == ChessGame.TeamColor.WHITE)? WHITE_BISHOP : BLACK_BISHOP;
+          case KNIGHT -> pieceStr = (piece.getTeamColor() == ChessGame.TeamColor.WHITE)? WHITE_KNIGHT : BLACK_KNIGHT;
+          case ROOK -> pieceStr = (piece.getTeamColor() == ChessGame.TeamColor.WHITE)? WHITE_ROOK : BLACK_ROOK;
+          case PAWN -> pieceStr = (piece.getTeamColor() == ChessGame.TeamColor.WHITE)? WHITE_PAWN : BLACK_PAWN;
+          case null -> pieceStr = EMPTY;
+        }
+        String squareResult;
+
+        ChessPosition currentPosition = new ChessPosition(rowNum, c + 1);
+        ChessMove currentMove = new ChessMove(highlightPosition, currentPosition, null);
+        if (piece != null) {
+          if ((r == 7 && piece.getTeamColor() == ChessGame.TeamColor.WHITE) || (r == 0 && piece.getTeamColor() == ChessGame.TeamColor.BLACK)) {
+            currentMove = new ChessMove(highlightPosition, currentPosition, ChessPiece.PieceType.QUEEN);
+          }
+        } else {
+
+        }
+
+        String setBgColor = ((rowNum + c) % 2 == 0)? SET_BG_COLOR_WHITE : SET_BG_COLOR_MAGENTA;
+        if (currentPosition.getRow() == highlightPosition.getRow() && currentPosition.getColumn() == highlightPosition.getColumn()) {
+          setBgColor = SET_BG_COLOR_YELLOW;
+        }
+        else if (validMoves.contains(currentMove)) {
+          setBgColor = ((rowNum + c) % 2 == 0)? SET_BG_COLOR_GREEN : SET_BG_COLOR_DARK_GREEN;
+        }
 
         squareResult = setBgColor + " " + pieceStr + " ";
         rowStr += squareResult;
