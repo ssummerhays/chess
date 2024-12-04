bb35d53ae4076e8eb4e165b7082d6cefbeedae27
ssummerhays <spencersummerhays3@gmail.com>
2024-12-04 14:17:40 -0700

    Created a NotificationHandler interface and updated the repl to implement that NotificationHandler class

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,11 +1,16 @@
 package ui;
 
+import websocket.NotificationHandler;
+import websocket.messages.Notification;
+
 import java.util.Scanner;
 
-public class Repl {
+import static ui.EscapeSequences.ERASE_LINE;
+
+public class Repl implements NotificationHandler {
   private final ChessClient client;
 
-  public Repl(String serverURL) { client = new ChessClient(serverURL);}
+  public Repl(String serverURL) { client = new ChessClient(serverURL, this);}
 
   public void run() {
     System.out.println("♕ Welcome to 240 Chess. Type help to begin");
@@ -25,6 +30,12 @@ public class Repl {
       }
       System.out.println();
     }
+  }
+
+  public void notify(Notification notification) {
+    System.out.print(ERASE_LINE + '\r');
+    System.out.println(EscapeSequences.SET_TEXT_COLOR_RED + notification.getMessage());
+    printPrompt();
   }
 
   private void printPrompt() { System.out.print("\n" + EscapeSequences.RESET_TEXT_COLOR + "[" + client.state + "] >>> "); }
