7831c378271813002250859d5561cbd9535dae74
ssummerhays <spencersummerhays3@gmail.com>
2024-12-04 14:15:30 -0700

    Updated UserGameCommand to include a teamColor for the connect command so that it can provide information regarding what color or whether a user is an observer in the notification

--- shared/src/main/java/websocket/commands/UserGameCommand.java
+++ shared/src/main/java/websocket/commands/UserGameCommand.java
@@ -1,5 +1,7 @@
 package websocket.commands;
 
+import chess.ChessGame;
+
 import java.util.Objects;
 
 /**
@@ -16,10 +18,13 @@ public class UserGameCommand {
 
     private final Integer gameID;
 
-    public UserGameCommand(CommandType commandType, String authToken, Integer gameID) {
+    private final ChessGame.TeamColor color;
+
+    public UserGameCommand(CommandType commandType, String authToken, Integer gameID, ChessGame.TeamColor color) {
         this.commandType = commandType;
         this.authToken = authToken;
         this.gameID = gameID;
+        this.color = color;
     }
 
     public enum CommandType {
@@ -40,6 +45,8 @@ public class UserGameCommand {
     public Integer getGameID() {
         return gameID;
     }
+
+    public ChessGame.TeamColor getColor() { return color; }
 
     @Override
     public boolean equals(Object o) {
