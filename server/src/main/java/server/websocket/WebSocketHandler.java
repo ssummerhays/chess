fa39fd98a96db3396ffe0f4e856210b574fd1373
ssummerhays <spencersummerhays3@gmail.com>
2024-12-05 11:05:12 -0700

    Updated WebSocketHandler methods to correctly handle improper input

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -9,9 +9,12 @@ import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import websocket.commands.UserGameCommand;
+import websocket.messages.Error;
+import websocket.messages.LoadGame;
 import websocket.messages.Notification;
 
 import java.io.IOException;
+import java.util.Objects;
 
 @WebSocket
 public class WebSocketHandler {
@@ -34,63 +37,136 @@ public class WebSocketHandler {
     String authToken = command.getAuthToken();
 
     switch (command.getCommandType()) {
-      case CONNECT -> connect(authToken, session, command.getGameID(), command.getColor());
-      case LEAVE -> leave(authToken, session, command.getGameID(), command.getColor());
-      case RESIGN -> resign(authToken, session, command.getGameID(), command.getColor());
+      case CONNECT -> connect(authToken, session, command.getGameID());
+      case LEAVE -> leave(authToken, session, command.getGameID());
+      case RESIGN -> resign(authToken, session, command.getGameID());
     }
   }
 
-  private void connect(String authToken, Session session, int gameID, ChessGame.TeamColor color) throws IOException {
+  private void connect(String authToken, Session session, int gameID) throws IOException {
+    connections.add(authToken, session, gameID);
+    String username;
     try {
-      AuthData authData = authDAO.getAuth(authToken);
-      String username = authData.username();
-      String colorStr;
-      if (color == null) {
-        colorStr = "observer";
-      } else {
-        colorStr = (color == ChessGame.TeamColor.WHITE)? "white" : "black";
+      AuthData authData=authDAO.getAuth(authToken);
+      username=authData.username();
+    } catch (DataAccessException e) {
+      Error error = new Error("Error: Invalid AuthToken");
+      connections.sendError(authToken, error);
+      return;
+    }
+    GameData gameData;
+    try {
+      gameData=gameDAO.getGame(gameID);
+    } catch (DataAccessException e) {
+      Error error = new Error("Error: Game Does Not Exist");
+      connections.sendError(authToken, error);
+      return;
+    }
+
+    ChessGame.TeamColor color = null;
+    if (Objects.equals(username, gameData.whiteUsername())) {
+      color = ChessGame.TeamColor.WHITE;
+    } else if (Objects.equals(username, gameData.blackUsername())) {
+      color = ChessGame.TeamColor.BLACK;
+    }
+
+    String colorStr;
+    if (color == null) {
+      colorStr = "observer";
+    } else {
+      colorStr = (color == ChessGame.TeamColor.WHITE)? "white" : "black";
+    }
+
+
+    Notification notification = new Notification("%s has joined the game as %s".formatted(username, colorStr));
+    connections.broadcast(authToken, gameID, notification);
+
+    LoadGame loadGame = new LoadGame(gameData.game(), color);
+    connections.broadCastSelf(authToken, loadGame);
+  }
+
+  private void leave(String authToken, Session session, int gameID) throws IOException {
+    String username;
+    try {
+      AuthData authData=authDAO.getAuth(authToken);
+      username=authData.username();
+    } catch (DataAccessException e) {
+      Error error = new Error("Error: Invalid AuthToken");
+      connections.sendError(authToken, error);
+      return;
+    }
+    GameData gameData;
+    try {
+      gameData=gameDAO.getGame(gameID);
+    } catch (DataAccessException e) {
+      Error error = new Error("Error: Game Does Not Exist");
+      connections.sendError(authToken, error);
+      return;
+    }
+
+    ChessGame.TeamColor color = null;
+    if (Objects.equals(username, gameData.whiteUsername())) {
+      color = ChessGame.TeamColor.WHITE;
+    } else if (Objects.equals(username, gameData.blackUsername())) {
+      color = ChessGame.TeamColor.BLACK;
+    }
+
+    String result;
+    if (color == null) {
+      result = "Observer %s has left the game".formatted(username);
+    } else if (color == ChessGame.TeamColor.WHITE) {
+      result = "White Player %s has left the game".formatted(username);
+    } else {
+      result = "Black Player %s has left the game".formatted(username);
+    }
+    Notification notification = new Notification(result);
+    connections.broadcast(authToken, gameID, notification);
+    connections.remove(authToken);
+  }
+
+  private void resign(String authToken, Session session, int gameID) throws IOException {
+    try {
+      String username;
+      try {
+        AuthData authData=authDAO.getAuth(authToken);
+        username=authData.username();
+      } catch (DataAccessException e) {
+        Error error = new Error("Error: Invalid AuthToken");
+        connections.sendError(authToken, error);
+        return;
+      }
+      GameData gameData;
+      try {
+        gameData=gameDAO.getGame(gameID);
+      } catch (DataAccessException e) {
+        Error error = new Error("Error: Game Does Not Exist");
+        connections.sendError(authToken, error);
+        return;
       }
 
-      connections.add(authToken, session, gameID);
-      Notification notification = new Notification("%s has joined the game as %s".formatted(username, colorStr));
-      connections.broadcast(authToken, gameID, notification);
-    } catch (DataAccessException e) {
-      throw new IOException();
-    }
-  }
-
-  private void leave(String authToken, Session session, int gameID, ChessGame.TeamColor color) throws IOException {
-    try {
-      AuthData authData = authDAO.getAuth(authToken);
-      String username = authData.username();
-      String result;
-      if (color == null) {
-        result = "Observer %s has left the game".formatted(username);
-      } else if (color == ChessGame.TeamColor.WHITE) {
-        result = "White Player %s has left the game".formatted(username);
-      } else {
-        result = "Black Player %s has left the game".formatted(username);
+      ChessGame.TeamColor color = null;
+      if (Objects.equals(username, gameData.whiteUsername())) {
+        color = ChessGame.TeamColor.WHITE;
+      } else if (Objects.equals(username, gameData.blackUsername())) {
+        color = ChessGame.TeamColor.BLACK;
       }
-      Notification notification = new Notification(result);
-      connections.broadcast(authToken, gameID, notification);
-      connections.remove(authToken);
-    } catch (DataAccessException e) {
-      throw new IOException();
-    }
-  }
 
-  private void resign(String authToken, Session session, int gameID, ChessGame.TeamColor color) throws IOException {
-    try {
-      AuthData authData = authDAO.getAuth(authToken);
-      String username = authData.username();
-
-      GameData gameData = gameDAO.getGame(gameID);
-      String oppositeUsername = (color == ChessGame.TeamColor.WHITE)? gameData.blackUsername() : gameData.whiteUsername();
-      Notification notification = new Notification("%s resigns. %s has won the game!".formatted(username, oppositeUsername));
-      connections.broadcast(authToken, gameID, notification);
-      GameData newGameData = new GameData(gameData.gameID(), gameData.whiteUsername(), gameData.blackUsername(), gameData.gameName(), gameData.game(),
-              1);
-      gameDAO.updateGame(newGameData);
+      if (color == null) {
+        Error error = new Error("Error: Observer cannot resign");
+        connections.sendError(authToken, error);
+      }
+      else if (gameData.over() != 0) {
+        Error error = new Error("Error: This game is already over");
+        connections.sendError(authToken, error);
+      }
+      else {
+        String oppositeUsername=(color == ChessGame.TeamColor.WHITE) ? gameData.blackUsername() : gameData.whiteUsername();
+        Notification notification=new Notification("%s resigns. %s has won the game!".formatted(username, oppositeUsername));
+        connections.broadcast("", gameID, notification);
+        GameData newGameData=new GameData(gameData.gameID(), gameData.whiteUsername(), gameData.blackUsername(), gameData.gameName(), gameData.game(),
+                1);
+        gameDAO.updateGame(newGameData);
+      }
     } catch (DataAccessException e) {
       throw new IOException();
     }
