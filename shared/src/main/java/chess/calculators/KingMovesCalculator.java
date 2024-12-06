efb78b902b7cab862ac3c2034a6a1fafc89622b8
ssummerhays <spencersummerhays3@gmail.com>
2024-12-05 22:09:40 -0700

    Update KingMovesCalculator

--- shared/src/main/java/chess/calculators/KingMovesCalculator.java
+++ shared/src/main/java/chess/calculators/KingMovesCalculator.java
@@ -13,16 +13,14 @@ public class KingMovesCalculator {
     int currentCol=myPosition.getColumn();
 
     for (int r=currentRow - 1; r <= currentRow + 1; r++) {
-      if (r>=8 || r<1) {continue;}
+      if (r>8 || r<=1) {continue;}
       for (int c=currentCol - 1; c <= currentCol + 1; c++) {
-        if (c>=8 || c<1) {continue;}
+        if (c>8 || c<=1) {continue;}
         if (r != currentRow || c != currentCol) {
           ChessPosition endPosition=new ChessPosition(r, c);
           ChessMove move=new ChessMove(myPosition, endPosition, null);
           if (board.getPiece(endPosition) == null || board.getPiece(endPosition).getTeamColor() != piece.getTeamColor()) {
             moves.add(move);
-          } else {
-            break;
           }
         }
       }
